d82b4dc138ab74cc4e519e1c234aa3f1986839ae
khashab2 <khashab2@illinois.edu>
2017-03-08 23:01:57 -0600

    moving the test files.

--- src/test/org/cogcomp/DatastoreTest.java
+++ src/test/org/cogcomp/DatastoreTest.java
@@ -1,4 +1,4 @@
-package java.org.cogcomp;
+package org.cogcomp;
 
 import org.cogcomp.Datastore;
 import org.cogcomp.DatastoreException;
@@ -20,6 +20,7 @@ public class DatastoreTest {
     @Test
     public void test() throws DatastoreException {
         ds.publishFile("edu.cogcomp", "pom", 1.0, "pom.xml");
+
         File f = ds.getFile("edu.cogcomp", "pom", 1.0);
         assert f.exists();
     }
